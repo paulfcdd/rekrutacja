de4d37c876619e4c7e5889d38641d38a54e6dcec
Paul Novykov <pawelfcdd@gmail.com>
2026-01-11 11:04:26 +0100

    add api client

--- rest-client/src/ApiClient.php
+++ rest-client/src/ApiClient.php
@@ -3,7 +3,15 @@ declare(strict_types=1);
 
 namespace Paulnovikov\RestClient;
 
-class ApiClient
-{
+use Paulnovikov\RestClient\Http\HttpClientInterface;
+use Paulnovikov\RestClient\Producer\ProducerApi;
 
+final readonly class ApiClient
+{
+    public function __construct(private HttpClientInterface $httpClient) {}
+
+    public function producers(): ProducerApi
+    {
+        return new ProducerApi($this->httpClient);
+    }
 }
